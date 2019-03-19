e5aa6da9be69c588afe3ca732e081a94ca9c2a85
Erik Garces Matos <erik_senac@hotmail.com>
2019-03-19 17:18:33 -0300

    'oo'

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { createStackNavigator } from "react-navigation";
+import Main from "./pages/main";
+
+export default createStackNavigator({
+    Main
+},{navigationOptions:{
+    headerStyle:{
+backgroundColor:"#da552f"
+    },
+    headerTintColor:"#fff"
+}});
